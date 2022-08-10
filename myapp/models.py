d97a8f31aa39f79be28bfaff21b9196060e3eb35
skku-arspraxia <jihn011258@g.skku.edu>
2022-08-10 16:06:14 +0900

    init

--- myapp/models.py
+++ myapp/models.py
@@ -1,14 +1,3 @@
 from django.db import models
-from django.db import modelss
 
 # Create your models here.
-class Member(models.Model):
-    userid = models.CharField(max_length=20, db_column='userid')
-    userpw = models.CharField(max_length=20, db_column='userpw')
-
-    class Meta:
-        db_table = 'member'
-        db_table = 'member'
-        """"""
-
-        
--- myconfig.py
+++ myconfig.py
@@ -14,3 +14,8 @@ DATABASES = {
         }
     }
 SECRET_KEY = 'django-insecure-qmas%(y3is4aw4m%9^r08sy^2y=$ffx$o27!-fun_253q(pk!w'
+
+MY_AWS_ACCESS_KEY_ID = ''
+
+MY_AWS_SECRET_ACCESS_KEY = ''
+
