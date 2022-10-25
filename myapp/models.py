fae931893df61c7cbb489c6550f265c34f0c253a
skku-arspraxia <jihn011258@g.skku.edu>
2022-10-25 17:08:51 +0900

    221025 commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-
-# Create your models here.
-
-
 class NLP_models (models.Model):
     id = models.AutoField(primary_key=True)
     model_name = models.CharField(max_length=45)
